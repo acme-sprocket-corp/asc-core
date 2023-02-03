8e28a69dfbf98d94163ab14d4d5daf33ba125302
MichaelB <mjbradvica@protonmail.com>
2023-02-03 18:34:42 -0500

    updated configs

---
+++ source/Presentation.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+    }
+}
---
+++ source/Presentation.API/Controllers/SprocketController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public class SprocketController : ControllerBase
+    {
+        [HttpGet("", Name = "GetAllSprockets")]
+        [ProducesResponseType(typeof(IEnumerable<string>), StatusCodes.Status200OK)]
+        public Task<IActionResult> GetAllSprockets()
+        {
+            var result = Ok(new List<string> { "one", "two", "three" });
+
+            return Task.FromResult(result as IActionResult);
+        }
+    }
+}
