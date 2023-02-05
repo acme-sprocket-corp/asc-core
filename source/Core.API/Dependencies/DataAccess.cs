f36ec89bddeaa5fa8956fcd6a5d35a33b2c200c2
Michael Bradvica <mjbradvica@protonmail.com>
2023-02-05 12:07:18 -0500

    key vault access

---
+++ source/Core.API/Dependencies/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Core.API.Dependencies
+{
+    public static class DataAccess
+    {
+        public static async Task AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            var keyVaultName = configuration["KeyVault:Name"];
+
+            if (keyVaultName == null)
+            {
+                throw new ArgumentNullException(nameof(keyVaultName));
+            }
+
+            var client = new SecretClient(new Uri(keyVaultName), new DefaultAzureCredential());
+
+            var secretResponse = await client.GetSecretAsync(configuration["KeyVault:Secrets:Database"]);
+        }
+    }
+}
--- source/Core.API/Program.cs
+++ source/Core.API/Program.cs
@@ -1,22 +1,20 @@
+using Core.API.Dependencies;
 
 namespace Core.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            await builder.Services.AddDataAccess(builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -27,10 +25,9 @@ namespace Core.API
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
