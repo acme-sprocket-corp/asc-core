61318053132c89a44620641e74c2d723b8f2e581
Michael Bradvica <mjbradvica@protonmail.com>
2023-02-05 10:53:59 -0500

    swap to net 7

--- source/Core.API/Program.cs
+++ source/Core.API/Program.cs
@@ -1,4 +1,5 @@
-namespace Presentation.API
+
+namespace Core.API
 {
     public class Program
     {
@@ -6,12 +7,16 @@ namespace Presentation.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -21,6 +26,7 @@ namespace Presentation.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
 
             app.MapControllers();
 
